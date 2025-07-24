219afa26cec5d886b8669b2b321f038201baa0c7
johnny kwan <johnny.sh.kwan@gmail.com>
2025-07-24 11:45:34 +1000

    first commit

---
+++ WebApi/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    // Optional: Configure Swagger document info (title, version, etc.)
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Nfl Depth Chart Api",
+        Description = "Nfl Depth Chart Api - Swagger"
+    });
+});
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    Console.WriteLine("weatherforecast");
+
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ WebApi/README.md
@@ -0,0 +1,22 @@
+# NFL depth chart
+dotnet minimal api with swagger
+
+
+
+
+
+
+## future items
+
+clean architecture:
+
+```
+src/    domain  
+        application  
+        infrastructure  
+        api  
+
+tests/  UnitTests  
+        IntegrationTests
+```
+
