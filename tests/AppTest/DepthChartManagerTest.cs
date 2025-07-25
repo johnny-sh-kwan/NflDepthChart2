01b0a6c28e0c541a5cf7f19685e5b5443298cbf1
johnny kwan <johnny.sh.kwan@gmail.com>
2025-07-25 12:31:40 +1000

    fix test, need to instantiate dcManager in the fact test. Add 1 more test scenario.

--- tests/AppTest/DepthChartManagerTest.cs
+++ tests/AppTest/DepthChartManagerTest.cs
@@ -8,12 +8,11 @@ namespace AppTest;
 public class DepthChartManagerTest
 {
     private readonly Mock<ILiteDbRepo> _repoMock;
-    private readonly DepthChartManager _manager;
+    private DepthChartManager? _manager;
 
     public DepthChartManagerTest()
     {
         _repoMock = new Mock<ILiteDbRepo>();
-        _manager = new DepthChartManager(_repoMock.Object);
     }
 
     [Fact]
@@ -21,6 +20,7 @@ public class DepthChartManagerTest
     {
         // Arrange: Mock the Load method to return an empty DepthChart
         _repoMock.Setup(r => r.Load()).Returns(new DepthChart { Chart = new Dictionary<Position, List<Player>>() });
+        _manager = new DepthChartManager(_repoMock.Object);
         var player = new Player { Name = "Tom Brady", Number = 12 };
 
         // Act
@@ -33,9 +33,9 @@ public class DepthChartManagerTest
 
         _repoMock.Verify(r => r.Save(It.IsAny<DepthChart>()), Times.Once);
     }
-    
+
     [Fact]
-    public void AddPlayerToDepthChart_AddsPlayerToEnd_WhenPositionExists()
+    public void AddPlayerToDepthChart_AddsPlayer_WhenPositionExists_ToEnd()
     {
         // Arrange: Mock the Load method to return an empty DepthChart
         _repoMock.Setup(r => r.Load()).Returns(new DepthChart
@@ -45,6 +45,7 @@ public class DepthChartManagerTest
                 { Position.QB, new List<Player>() { new Player { Name = "John Doe", Number = 1 } } }
             }
         });
+        _manager = new DepthChartManager(_repoMock.Object);
         var player = new Player { Name = "Tom Brady", Number = 12 };
 
         // Act
@@ -57,6 +58,34 @@ public class DepthChartManagerTest
         _manager.DepthChart.Chart[Position.QB].First().Number.Should().Be(1);
         _manager.DepthChart.Chart[Position.QB].Skip(1).First().Name.Should().Be("Tom Brady");
         _manager.DepthChart.Chart[Position.QB].Skip(1).First().Number.Should().Be(12);
+
+        _repoMock.Verify(r => r.Save(It.IsAny<DepthChart>()), Times.Once);
+    }
+    
+    [Fact]
+    public void AddPlayerToDepthChart_AddsPlayer_WhenPositionExists_ToFront()
+    {
+        // Arrange: Mock the Load method to return an empty DepthChart
+        _repoMock.Setup(r => r.Load()).Returns(new DepthChart
+        {
+            Chart = new Dictionary<Position, List<Player>>()
+            {
+                { Position.QB, new List<Player>() { new Player { Name = "John Doe", Number = 1 } } }
+            }
+        });
+        _manager = new DepthChartManager(_repoMock.Object);
+        var player = new Player { Name = "Tom Brady", Number = 12 };
+
+        // Act
+        _manager.AddPlayerToDepthChart(Position.QB, player, 0);
+
+        // Assert
+        _manager.DepthChart.Chart.Should().ContainKey(Position.QB);
+
+        _manager.DepthChart.Chart[Position.QB].First().Name.Should().Be("Tom Brady");
+        _manager.DepthChart.Chart[Position.QB].First().Number.Should().Be(12);
+        _manager.DepthChart.Chart[Position.QB].Skip(1).First().Name.Should().Be("John Doe");
+        _manager.DepthChart.Chart[Position.QB].Skip(1).First().Number.Should().Be(1);
 
         _repoMock.Verify(r => r.Save(It.IsAny<DepthChart>()), Times.Once);
     }
